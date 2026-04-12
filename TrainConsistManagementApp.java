0c221f94a73edd9236c312190683c708528f2592
DIVI-ai <divitakapoor22@gmail.com>
2026-04-12 10:07:16 +0530

    USE CASE 2

--- TrainConsistManagementApp.java
+++ TrainConsistManagementApp.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class TrainConsistManagementApp {
+public class TrainApp {
 
     public static void main(String[] args) {
 
@@ -11,9 +11,36 @@ public class TrainConsistManagementApp {
         // Initialize empty list of bogies
         List<String> bogies = new ArrayList<>();
 
-        // Display initial bogie count
+        // UC1 Output
         System.out.println("Train consist initialized.");
         System.out.println("Initial number of bogies: " + bogies.size());
 
+        // ---------------- UC2 START ----------------
+
+        // Adding passenger bogies
+        bogies.add("Sleeper Bogie");
+        bogies.add("AC Chair Bogie");
+        bogies.add("First Class Bogie");
+
+        System.out.println("\nAfter adding passenger bogies:");
+        System.out.println("Total number of bogies: " + bogies.size());
+        System.out.println("Current Train Consist: " + bogies);
+
+        // Checking if a bogie exists
+        String checkBogie = "AC Chair Bogie";
+        if (bogies.contains(checkBogie)) {
+            System.out.println(checkBogie + " is present in the train.");
+        } else {
+            System.out.println(checkBogie + " is NOT present in the train.");
+        }
+
+        // Removing a bogie
+        bogies.remove("Sleeper Bogie");
+
+        System.out.println("\nAfter removing Sleeper Bogie:");
+        System.out.println("Total number of bogies: " + bogies.size());
+        System.out.println("Updated Train Consist: " + bogies);
+
+        // ---------------- UC2 END ----------------
     }
 }
