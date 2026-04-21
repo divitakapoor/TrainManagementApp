000f2c7e756ed4f7cfe9344e688640b5dc3cae7b
DIVI-ai <divitakapoor22@gmail.com>
2026-04-22 00:42:10 +0530

    USE CASE 9

--- TrainConsistManagementApp.java
+++ TrainConsistManagementApp.java
@@ -1,38 +1,54 @@
 import java.util.*;
 import java.util.stream.Collectors;
 
+class Bogie {
+    String id;
+    String type;     // Sleeper, AC Chair, First Class, Goods
+    int capacity;
+
+    public Bogie(String id, String type, int capacity) {
+        this.id = id;
+        this.type = type;
+        this.capacity = capacity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String toString() {
+        return id + "(" + capacity + ")";
+    }
+}
+
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
         System.out.println("=== Train Consist Management App ===");
 
-        // UC6 data (Map)
-        Map<String, Integer> bogieCapacity = new HashMap<>();
+        // Create bogie objects
+        List<Bogie> bogies = new ArrayList<>();
 
-        bogieCapacity.put("BG101", 72);  // Sleeper
-        bogieCapacity.put("BG102", 56);  // AC Chair
-        bogieCapacity.put("BG103", 24);  // First Class
-        bogieCapacity.put("BG104", 80);  // Extra High Capacity
+        bogies.add(new Bogie("BG101", "Sleeper", 72));
+        bogies.add(new Bogie("BG102", "AC Chair", 56));
+        bogies.add(new Bogie("BG103", "First Class", 24));
+        bogies.add(new Bogie("BG104", "Sleeper", 80));
+        bogies.add(new Bogie("BG105", "Goods", 100));
 
-        System.out.println("\nAll Bogies:");
-        System.out.println(bogieCapacity);
+        // ---------------- UC9 START ----------------
 
-        // ---------------- UC8 START ----------------
+        // Group bogies by type
+        Map<String, List<Bogie>> groupedBogies =
+                bogies.stream()
+                        .collect(Collectors.groupingBy(Bogie::getType));
 
-        // Filter bogies with capacity >= 50
-        List<Map.Entry<String, Integer>> highCapacityBogies =
-                bogieCapacity.entrySet()
-                        .stream()
-                        .filter(entry -> entry.getValue() >= 50)
-                        .collect(Collectors.toList());
+        System.out.println("\nGrouped Bogies:");
 
-        System.out.println("\nHigh Capacity Bogies (>= 50 seats):");
-        for (Map.Entry<String, Integer> entry : highCapacityBogies) {
-            System.out.println("Bogie ID: " + entry.getKey()
-                    + " → Capacity: " + entry.getValue());
+        for (String type : groupedBogies.keySet()) {
+            System.out.println(type + " Bogies: " + groupedBogies.get(type));
         }
 
-        // ---------------- UC8 END ----------------
+        // ---------------- UC9 END ----------------
     }
 }
